96a7d44905ba9d5953b35b0811d6614b5469bdbc
Nick <nick.gill@fjorgedigital.com>
2021-03-11 15:13:29 -0600

    inital project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { ChatEngine } from 'react-chat-engine';
+
+function App() {
+  return (
+    <ChatEngine
+      height='100vh'
+      userName='ngill'
+      userSecret='123123'
+      projectID='91a4b4ea-3d84-4d08-bec4-7281d41b85c3'
+    />
+  );
+}
+
+export default App;
